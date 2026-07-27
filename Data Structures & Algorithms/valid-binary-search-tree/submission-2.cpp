55ca717d607c4cf26135c12199474f6dd7da3d71
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-07-26 21:50:03 -0400

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* node, long minVal = LONG_MIN, long maxVal = LONG_MAX) {
+        if(!node)
+            return true;
+
+        if(node->val <= minVal || node->val >= maxVal)
+            return false;
+        
+        return isValidBST(node->left, minVal, node->val) && 
+               isValidBST(node->right, node->val, maxVal);
+    }
+};

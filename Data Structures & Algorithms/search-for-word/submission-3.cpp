5a398e20169a9daac4b36a792cc97f261bd9c60a
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-08-13 17:11:05 -0400

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    vector<vector<char>>* boardPtr;
+    string word;
+    int rows;
+    int cols;
+
+    bool dfs(int r, int c, int i) {
+        vector<vector<char>>& board = *boardPtr;      // local alias, zero cost
+        if (i == word.size()) return true;
+        if(r<0 || r>=rows || c<0 || c>=cols || board[r][c] == '#') return false;
+        if(board[r][c] != word[i]) return false;
+        
+        char original = board[r][c];
+        board[r][c] = '#'; //visted so mark it
+        bool found = dfs(r+1, c, i+1) || 
+            dfs(r-1, c, i+1) || 
+            dfs(r, c+1, i+1) || 
+            dfs(r, c-1, i+1);
+        board[r][c] = original; //unmark it
+        return found;
+    }
+
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        boardPtr = &board;
+        this->word = move(word);
+        rows = board.size();
+        cols = board[0].size();
+
+        for(int r=0; r<rows; r++)
+            for(int c=0; c<cols; c++)
+                if(dfs(r, c, 0)) return true;
+        return false; 
+    }
+};

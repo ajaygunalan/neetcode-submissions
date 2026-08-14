1c28d100d6025761082811092da15de2ae5a4e7e
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-08-13 21:56:53 -0400

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+    vector<vector<int>> result;
+    vector<int> path;
+    vector<int>* numsPtr;
+    
+    void backtrack(int i) {
+        vector<int>& nums = *numsPtr;
+        result.push_back(path);
+        while(i < nums.size()) {
+            path.push_back(nums[i]);
+            backtrack(i+1);   
+            path.pop_back();
+            i++;
+        }
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        numsPtr = &nums;
+        backtrack(0);
+        return result;
+    }
+};
+
+
+
+// choose 1 -> [1], 
+// --------- choose 2 -> [1, 2],  
+//-------------------------------choose 3- >[1, 2, 3]
+// ----------- unchoose 2 -> [1, 3]
+
+
+// unchoose 1 -> 
+//--------- choose 2 ->  [2], 
+//-----------------------------choose 3 -> [2,3]
+//--------- unchoose 2 ->  [3]

fb2d3c52da5238e1fb06342bbad52bf23086e132
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-08-17 15:14:58 -0400

    Add: longest-palindromic-substring - submission-6

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-6.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    string s;
+    vector<vector<char>> dp;          // was vector<vector<bool>>
+
+    bool isPalindrome(int l, int r) {
+        if (s[l] != s[r]) return false;
+        int currentLen = r - l + 1;
+        if (currentLen <= 3) return true;
+        return dp[l + 1][r - 1];      // char → bool conversion is implicit
+    }
+
+public:
+    string longestPalindrome(string s) {
+        this->s = std::move(s);
+        int n = this->s.size();
+        dp.assign(n, vector<char>(n, 0));    // was vector<bool>(n, false)
+
+        int bestStart = 0, bestLen = 0;
+
+        for (int l = n - 1; l >= 0; l--) {
+            for (int r = l; r < n; r++) {
+                dp[l][r] = isPalindrome(l, r);
+                if (dp[l][r]) {
+                    int currentLen = r - l + 1;
+                    if (currentLen > bestLen) {
+                        bestStart = l;
+                        bestLen = currentLen;
+                    }
+                }
+            }
+        }
+        return this->s.substr(bestStart, bestLen);
+    }
+};

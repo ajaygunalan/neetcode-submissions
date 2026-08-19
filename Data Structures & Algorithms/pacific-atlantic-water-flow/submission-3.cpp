1ad1178116d2ba8a581898267640b589e0bc2e6b
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-08-19 10:20:57 -0400

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+private:
+    vector<vector<int>> heights;
+    vector<vector<bool>> pacific;
+    vector<vector<bool>> atlantic;
+    int m, n;
+
+    void dfs(int r, int c, int prevHeight, vector<vector<bool>>& ocean) {
+        if (r < 0 || r >= m || c < 0 || c >= n) return;
+        if (heights[r][c] < prevHeight) return;
+        if (ocean[r][c]) return;
+        ocean[r][c] = true;
+        dfs(r + 1, c, heights[r][c], ocean);
+        dfs(r - 1, c, heights[r][c], ocean);
+        dfs(r, c + 1, heights[r][c], ocean);
+        dfs(r, c - 1, heights[r][c], ocean);
+    }
+
+public:
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        this->heights = heights;
+        m = heights.size();
+        n = heights[0].size();
+
+        pacific  = vector<vector<bool>>(m, vector<bool>(n, false));
+        atlantic = vector<vector<bool>>(m, vector<bool>(n, false));
+
+        for (int r = 0; r < m; r++) {
+            dfs(r, 0, 0, pacific);
+            dfs(r, n - 1, 0, atlantic);
+        }
+        for (int c = 0; c < n; c++) {
+            dfs(0, c, 0, pacific);
+            dfs(m - 1, c, 0, atlantic);
+        }
+
+        vector<vector<int>> result;
+        for (int r = 0; r < m; r++)
+            for (int c = 0; c < n; c++)
+                if (pacific[r][c] && atlantic[r][c])
+                    result.push_back({r, c});
+
+        return result;
+    }
+};

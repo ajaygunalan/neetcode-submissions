36f7644d2eb085390415a339732cca9aba3f75fe
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-08-22 18:08:17 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    unordered_map<int, vector<int>> graph;
+    vector<int> state;
+
+    bool dfs(int course) {
+        if(state[course] == 1) return false;
+        if(state[course] == 2) return true;
+        state[course] = 1;
+
+        for(int neighbor : graph[course])
+            if(!dfs(neighbor))
+                return false;
+        state[course] = 2;
+        return true;
+    }
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        state.assign(numCourses, 0);
+        //0 means unvisited
+        //1 means visiting
+        //2 means visited 
+        
+        //build the graph
+        for(auto& pair : prerequisites)
+            graph[pair[0]].push_back(pair[1]);
+
+        //traverse the graph
+        for(int course=0; course<numCourses; course++)
+            if(!dfs(course))
+                return false;
+        return true;
+    }
+};

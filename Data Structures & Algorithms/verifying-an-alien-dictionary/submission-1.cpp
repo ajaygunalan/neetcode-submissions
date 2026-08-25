db8cbda8775e5ffdfb750b5a8d2c7874d7edef34
Ajay Gunalan <ajaygunalan1995@gmail.com>
2026-08-25 14:30:51 -0400

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> hashMap;
+        for(int i=0; i<order.length(); i++) hashMap[order[i]] = i;
+    
+        size_t n = words.size();
+        for(auto const& [w1, w2] :  words | views::adjacent<2>) {
+            size_t len = min(w1.size(), w2.size());
+            size_t j=0;
+            while(j<len && w1[j] == w2[j] ) j++;
+            if(j == len && w1.size() > w2.size()) return false;
+            else if(hashMap[w1[j]] > hashMap[w2[j]]) return false;
+        }
+        return true;
+    }
+};
